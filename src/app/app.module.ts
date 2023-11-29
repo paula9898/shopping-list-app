0dd1d65ff957d74bc772382247b7377a6e18af5e
paula9898 <otrebskapaulina@gmail.com>
2023-11-29 21:57:43 +0100

    Add recipe-list-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
     RecipesComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
   ],
   imports: [BrowserModule],
   providers: [],
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {
-
-}
+export class RecipeListComponent {}
