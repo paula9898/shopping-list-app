8514fdb8462cb5ab4eb7fdd2a162c27e1f816340
paula9898 <otrebskapaulina@gmail.com>
2023-12-03 16:03:39 +0100

    create recipe detail, add ingredient model, show shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('egg', 3),
+    new Ingredient('tomato', 5),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
