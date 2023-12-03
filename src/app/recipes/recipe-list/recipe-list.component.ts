57e382e45b38ebadd52c806fd2173a53f7160c14
paula9898 <otrebskapaulina@gmail.com>
2023-12-03 14:38:45 +0100

    Add list with recipes and add recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg?quality=90&resize=500,454'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
